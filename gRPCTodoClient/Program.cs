c30a8b55c1a6d59b3d1d3561238bd8744ac39ea6
Ghalia <ghaliadahech@gmail.com>
2025-07-02 11:12:45 +0300

    Added: gRPC client

---
+++ gRPCTodoClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using Grpc.Net.Client;
+using ToDoListApp;
+using ToDoListApp;
+
+var channel = GrpcChannel.ForAddress("https://localhost:7094");
+var client = new ToDo.ToDoClient(channel);
+
+Console.WriteLine("Введите задачу");
+var title = Console.ReadLine();
+
+var addResponse = await client.AddToDoAsync(new AddToDoRequest { Title = title });
+Console.WriteLine($"Добавлено: {addResponse.Item.Id}: {addResponse.Item.Title}");
+
+var list = await client.GetAllToDosAsync(new GetAllToDosRequest());
+Console.WriteLine("Список задач:");
+foreach (var item in list.Items)
+{
+    var status = item.IsDone ? "[✓]" : "[ ]";
+    Console.WriteLine($"{item.Id}. {item.Title}: {status}");
+}
+
+Console.WriteLine("Ввудите ID задачи, чтобы отметить выполненной: ");
+if (int.TryParse(Console.ReadLine(), out int idToMark))
+{
+    var result = await client.MarkDoneAsync(new MarkDoneRequest { Id = idToMark });
+    Console.WriteLine(result.Success ? "Успешно!" : "Не найдено.");
+}
+
+Console.ReadLine();
